d1f9c2f645bde30bfec83eaab6565fda2db9dab1
Hari-singh-Joshi <132601298+Hari-singh-Joshi@users.noreply.github.com>
2025-07-06 15:42:50 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 const nunito = Nunito({ subsets: ["latin"], variable: "--font-nunito" })
-import Script from 'next/script';
+
 export const metadata: Metadata = {
   title: "NotesFy - Your Ultimate Source for BTech CSE Notes",
   description:
@@ -17,18 +17,18 @@ export const metadata: Metadata = {
   authors: [{ name: "NotesFy Team" }],
   openGraph: {
     type: "website",
-    url: "https://notes-fy.vercel.app/",
+    url: "https://notesfy.vercel.app/",
     title: "NotesFy - BTech CSE Study Material | UTU/Tula's Notes & Handwritten Notes",
     description:
       "Discover the best BTech CSE study resources with NotesFy. Tula's notes, handwritten notes, UTU materials, and more for engineering students.",
-    images: [{ url: "https://notes-fy.vercel.app/assets/img/favicon.jpg" }],
+    images: [{ url: "https://notesfy.vercel.app/assets/img/favicon.jpg" }],
   },
   twitter: {
     card: "summary_large_image",
     title: "NotesFy - BTech CSE Study Material | UTU/Tula's Notes & Handwritten Notes",
     description:
       "Your one-stop destination for BTech CSE notes, UTU/Tula's notes, handwritten notes, and UTU study materials. Visit NotesFy today!",
-    images: ["https://notes-fy.vercel.app/assets/img/favicon.jpg"],
+    images: ["https://notesfy.vercel.app/assets/img/favicon.jpg"],
   },
     generator: 'v0.dev'
 }
@@ -42,11 +42,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="google-adsense-account" content="ca-pub-7913957132300666" />
-        <link rel="canonical" href="https://notes-fy.vercel.app/" />
         <link rel="icon" href="/img/favicon.jpg" />
         <link rel="apple-touch-icon" href="/img/favicon.jpg" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7913957132300666"
-     crossorigin="anonymous"></script>
+        <link rel="canonical" href="https://notesfy.vercel.app/" />
+
       </head>
       <body className={`${inter.variable} ${nunito.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
